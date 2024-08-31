e0fe13480af10798e4c8eb50fd0a4c1abc88462c
hajime.takeuchi <shadowthe8156@gmail.com>
2024-08-31 14:30:42 +0900

    add logic sample todo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const todoInputTextBox = document.getElementById("input-todo");
 const todoAddButton = document.getElementById("add-todo");
+const todoList = document.getElementById("todo-list");
 
 todoAddButton.addEventListener('click', ()=>{
   const val = todoInputTextBox.value;
@@ -8,6 +9,12 @@ todoAddButton.addEventListener('click', ()=>{
     alert("TODOが入力されていません");
     return;
   }
+
+  const listItem = document.createElement('li');
+  listItem.className = "todo-li";
+  listItem.innerHTML = "ほげほげ";
+
+  todoList.appendChild(listItem);
 
   // TODO作成処理
   console.log(val);
