2ee8b0f448bbd38f1022a376baf49f23bb5977be
hajime.takeuchi <shadowthe8156@gmail.com>
2024-08-31 14:43:18 +0900

    add comments

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
+// id="input-todo"の要素を取得
 const todoInputTextBox = document.getElementById("input-todo");
+
+// id="add-todo"の要素を取得
 const todoAddButton = document.getElementById("add-todo");
+
+// id="todo-list"の要素を取得
 const todoList = document.getElementById("todo-list");
 
-todoAddButton.addEventListener('click', ()=>{
+// todoAddButton がClickされたときの処理
+todoAddButton.addEventListener("click", ()=>{
+  // テキストボックスの中身を取得
   const val = todoInputTextBox.value;
 
+  // テキストボックスの中身が空なら、alertを出して処理終了
   if (val == "") {
     alert("TODOが入力されていません");
     return;
   }
 
-  const listItem = document.createElement('li');
+  // <li class="todo-li"> 要素を作成
+  const listItem = document.createElement("li");
   listItem.className = "todo-li";
   listItem.innerHTML = "ほげほげ";
 
+  // todoListにlistItemを子要素として追加
   todoList.appendChild(listItem);
 
   // TODO作成処理
   console.log(val);
 
+  // テキストボックスの中身を空にする
   todoInputTextBox.value = "";
 });
